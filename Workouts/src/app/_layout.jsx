656f80c059adeaf2c70c0b50a65d6550d0bb4136
Olusegun Orija <oluorija@gmail.com>
2024-07-04 20:33:50 +0100

    Update _layout.jsx

--- Workouts/src/app/_layout.jsx
+++ Workouts/src/app/_layout.jsx
@@ -5,14 +5,10 @@ import { SplashScreen, Stack } from "expo-router";
 SplashScreen.preventAutoHideAsync();
 
 
-const RootLayout = () => {
+export default function RootLayout () {
     return (
         <Stack>
             <Stack.Screen name="index" options={{headerShown: false, title: 'Exercises'}}/>
         </Stack>
-    )
+    );
 }
-
-export default RootLayout
-
-const styles = StyleSheet.create({})
