0ff1a261a302a231e79a3b4325c8327dd81dedfb
Olusegun Orija <oluorija@gmail.com>
2024-07-08 22:12:55 +0100

    Working State Commit
    Working State Commit: using local exercises data

--- Workouts/src/app/_layout.jsx
+++ Workouts/src/app/_layout.jsx
@@ -1,14 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native'
 import { Stack } from "expo-router";
-import { ApolloProvider } from '@apollo/client';
-import client from '../api/ApolloClient';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+
+const client = new QueryClient();
 
 export default function RootLayout () {
     return (
-        <ApolloProvider client={client}>
+        <QueryClientProvider client={client}>
             <Stack>
                 <Stack.Screen name="index" options={{headerShown: false, title: 'Exercises'}}/>
             </Stack> 
-        </ApolloProvider>
+        </QueryClientProvider>
     );
 }
--- Workouts/src/app/index.jsx
+++ Workouts/src/app/index.jsx
@@ -1,34 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
-//import exercises from '../../assets/data/exercises.json';
+import exercises from '../../assets/data/exercises.json';
 import ExerciseListItem from '../components/ExerciseListItem';
-import { gql, useQuery } from '@apollo/client';
-// const url = 'http://localhost:4000/graphql'
-//const url = 'http://192.168.112.1:4000/graphql'
+import { useQuery } from '@tanstack/react-query';
+
+
 const url = 'http://localhost:4000/graphql'
 
 
-
-const GET_EXERCISES = gql`
-  query GetExercises($name: String, $type: String, $muscle: String, $difficulty: String, $limit: Int) {
-    exercises(name: $name, type: $type, muscle: $muscle, difficulty: $difficulty, limit: $limit) {
-      name
-      type
-      muscle
-      equipment
-      difficulty
-      instructions
-    }
-  }
-`;
-
-
 export default function ExercisesScreen() {
-  const { loading, error, data } = useQuery(GET_EXERCISES, {
-    variables: { name: "push-up", type: "strength", muscle: "chest", difficulty: "beginner", limit: 5 },
+  const {data, isLoading, error} = useQuery({
+    queryKey: ['exercises'],
+    queryFn: async () => {
+      return exercises;
+    },
   });
 
-  if(loading){
+  if(isLoading){
     return <ActivityIndicator/>
   }
 
@@ -36,13 +24,11 @@ export default function ExercisesScreen() {
     console.log(error);
     return <Text>Error: {error.message}</Text>;
   }
-  //const exercise = exercises[1];
-  const exercises = data?.exercises;
 
   return (
     <View style={styles.container}>
       <FlatList
-          data={exercises}
+          data={data}
           contentContainerStyle={{gap: 10}}
           keyExtractor={(item, index) => item.name + index}
           renderItem={({item}) => <ExerciseListItem item={item}/>}
