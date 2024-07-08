87912664063b43ea803293adc87ca0fdde9a8527
Olusegun Orija <oluorija@gmail.com>
2024-07-08 22:41:40 +0100

    Issues with graphql-request fixed by downgrading graphql and graphql-request
    REF: https://github.com/nrwl/nx/discussions/26312#discussioncomment-9918975
    
    Issues with graphql-request fixed by downgrading graphql and graphql-request

--- Workouts/src/app/index.jsx
+++ Workouts/src/app/index.jsx
@@ -1,18 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
-import exercises from '../../assets/data/exercises.json';
+//import exercises from '../../assets/data/exercises.json';
 import ExerciseListItem from '../components/ExerciseListItem';
 import { useQuery } from '@tanstack/react-query';
+import { gql, request } from 'graphql-request'
 
+const exercisesQuery = gql`
+  query exercises($muscle: String, $name: String) {
+  exercises(muscle: $muscle, name: $name) {
+    name
+	  muscle
+  }
+} 
+`;
 
-const url = 'http://localhost:4000/graphql'
+const url = 'https://btncibogo.us-east-a.ibm.stepzen.net/api/exercises/graphql'
 
 
 export default function ExercisesScreen() {
   const {data, isLoading, error} = useQuery({
     queryKey: ['exercises'],
     queryFn: async () => {
-      return exercises;
+      return request({url, document: exercisesQuery, requestHeaders: {"Authorization": "apikey btncibogo::local.net+1000::ac6a41cf812d9aeaf7981d590dd9ed3bab11a7cf3203ea62b14393196fde699b"}});
     },
   });
 
@@ -28,7 +37,7 @@ export default function ExercisesScreen() {
   return (
     <View style={styles.container}>
       <FlatList
-          data={data}
+          data={data?.exercises}
           contentContainerStyle={{gap: 10}}
           keyExtractor={(item, index) => item.name + index}
           renderItem={({item}) => <ExerciseListItem item={item}/>}
